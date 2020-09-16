b9f6bbc6340d1aaa7fb2652413e34029940e5efd
NKN1396 <someone.took.nkn1396@gmail.com>
2020-09-16 08:18:44 +0200

    Removed bot + modularization
    nova-core no longer acts as a standalone client loading commands provided by nova-makeshift or nova-public. Instead nova-core now provides core commands to nova-makeshift and nova-public, so these two can load commands from nova-core independently.

--- index.js
+++ index.js
@@ -1,49 +1,15 @@
 //External dependencies
-const commando = require("discord.js-commando")
 const discordErrorHandler = require("discord.js-handles")
 
-module.exports = function(token, handlerOptions) {
+module.exports = function(client) {
 
-	console.log("Bot initialization started")
+	console.log("nova-core: initializing")
 
-	//Create new Discord client
-	const bot = new commando.Client({
-		commandPrefix : "!",
-		unknownCommandResponse : false,
-		owner : "153595272465743872"
-	})
-
-	//Initialize bot error handling
-	discordErrorHandler(bot, handlerOptions)
-	
 	//Load custom features
-	//require("./src/modules/index")(bot)
+	//require("./src/modules/index")(client)
 
 	//Load and register commands.
-	//require("./commands/index")(bot)
+	require("./commands/index")(client)
 
-	//Start bot
-	bot.login(token)
-		.catch(console.error)
-
-	//Set Status
-	bot
-		.on("ready", () => {
-			setBotStatus(bot.user)
-		})
-		.on("resume", ()=> {
-			setBotStatus(bot.user)
-		})
 	
-	return bot
-}
-
-//Set bot status
-function setBotStatus(user){
-	user.setPresence({
-		activity: {
-			name: "@Nova help",
-			type: 0
-		}
-	})
 }
